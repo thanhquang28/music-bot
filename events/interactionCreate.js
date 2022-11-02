9e3469259b9a8d9b87a78d261e4cbb92d1763c09
Thành Quang <thanhquang2805@gmail.com>
2022-11-02 22:00:47 +0700

    update

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -82,7 +82,7 @@ module.exports = (client, inter) => {
           new EmbedBuilder()
             .setColor('#ff0000')
             .setDescription(
-              `❌ | You can only use commands in <#music-commands>`
+              `❌ | You can only use commands in <#music-command>`
             ),
         ],
         ephemeral: true,
